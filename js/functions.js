fc421c28b6e28018b4edbc90bb86485a494b6855
Lola <xuefangaau@gmail.com>
2020-04-21 14:13:24 -0700

    Change qrcode image and add click-copy function

--- js/functions.js
+++ js/functions.js
@@ -6,4 +6,21 @@ $(document).ready(function() {
 	$('#menuApp_closeBtn').click(function() {
 		$('#appMenu').addClass('menuApp_displayNone');	
 	})
+
+	$("#copy").on("tap",function(){
+	    var val = $("#textAreas").val();
+	    Clipboard.copy(val);
+	});
 })
+
+
+function myFunction() {
+  var copyText1 = document.getElementById("myInput1");
+  var copyText2 = document.getElementById("myInput2");
+  copyText1.select();
+  copyText2.select();
+  copyText1.setSelectionRange(0, 99999)
+  copyText2.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied!");
+}
